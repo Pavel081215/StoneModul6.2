b9d99a10385ac06b303cf21f2c6e034aa58f3498
pavel081215 <pavelzag12@gmail.com>
2016-05-28 13:35:24 +0300

    6.2  1 steps (setting  servlet-context and web.xml)

---
+++ src/main/java/entity/AccountController.java
@@ -0,0 +1,12 @@
+package entity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Pavel on 28.05.2016.
+ */
+@Controller
+@RequestMapping(value="/account")
+public class AccountController {
+}
