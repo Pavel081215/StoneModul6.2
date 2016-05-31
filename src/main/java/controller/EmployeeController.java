c19532421675fa402348f254ce0447e6bdeb45a8
pavel081215 <pavelzag12@gmail.com>
2016-05-31 14:24:04 +0300

    6.2  3 steps (revolution)

--- src/main/java/controller/EmployeeController.java
+++ src/main/java/controller/EmployeeController.java
@@ -1,26 +1,91 @@
 package controller;
-import java.util.Map;
 
 import dao.EmployeeService;
 import entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+
 @Controller
+@RequestMapping(value="/account")
 public class EmployeeController {
 
+  //  private Map<Long, Account> accounts = new ConcurrentHashMap<Long, Account>();
 
+    @RequestMapping(method=RequestMethod.GET)
+    public String getCreateForm(Model model) {
+        //	model.addAttribute(new Account());
+        return "new";
+        //return "account/createForm";
+    }
+
+
+/*
+@Controller
+@RequestMapping(value = "/")
+public class EmployeeController {
+
+    private Map<Long, Employee> accounts = new ConcurrentHashMap<Long, Employee>();
     public void setContactService(EmployeeService contactService) {
         this.contactService = contactService;
     }
 
     @Autowired
     private EmployeeService contactService;
+
+
+  //  @RequestMapping(method= RequestMethod.GET)
+  //  public String home() {
+//
+  //      System.out.println("<<<<<<<<<<<");
+    //    return "error403";
+    //}
+  @RequestMapping(method=RequestMethod.GET)
+  public String listContacts(Map<String, Object> map) {
+
+      map.put("contact", new Employee());
+      map.put("contactList", contactService.listEmplyee());
+
+      return "new";
+  }
+
+    @RequestMapping("/admin")
+    public String home() {
+        return "new";
+    }
+
+
+
+    @RequestMapping(method=RequestMethod.GET)
+    public String getCreateForm(Model model) {
+        model.addAttribute(new Employee());
+        return "new";
+    }
+
+    @RequestMapping(method=RequestMethod.POST)
+    public String create(@Valid Employee account, BindingResult result) {
+        if (result.hasErrors()) {
+            return "new";
+        }
+        this.accounts.put(account.getPhone(), account);
+        return "new" ;
+    }
+
+
+  /*   @RequestMapping(method = RequestMethod.GET)
+    public String getCreateForm(Model model) {
+        model.addAttribute(new Employee());
+        return "contact";
+    }
+
 
 
     @RequestMapping("/index")
@@ -54,5 +119,5 @@ public class EmployeeController {
         contactService.removeEmplyee(contactId);
 
         return "redirect:/index";
-    }
+    }*/
 }
